a1c2952e682918675b87a5ef4e3948bdc8284a4d
xvferdy <xv.ferdy@gmail.com>
2021-11-07 16:47:44 +0800

    JOJO

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // SCROLL EVENT
 const nav = document.querySelector("nav");
-const primary = "#8675a9";
-const secondary = "#efbbcf";
-const third = "#c3aed6";
+const primary = "#2b3838";
+const secondary = "#FFD24E";
+const third = "#acbf10";
+const dark = "#142141;";
 
 window.addEventListener("scroll", () => {
 	// nav border btml
 	if (pageYOffset > 500) {
-		nav.style.borderBottom = "1px dotted black";
+		nav.style.borderBottom = "1px dotted #2b3838";
 	} else if (pageYOffset < 500 && pageYOffset > 100) {
 		// nav.style.borderBottom = "none";
 		nav.style.borderBottom = "1px dotted transparent";
